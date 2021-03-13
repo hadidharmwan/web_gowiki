13d7b30e309a1c2d3b59ebee935f554fccfc525e
Kuja Dconnor <rubynapi321@gmail.com>
2021-03-14 00:38:03 +0700

    add

--- wiki1.go
+++ wiki1.go
@@ -12,7 +12,7 @@ type Page struct {
 	Body []byte
 }
 
-func (p * Page) save() error {
+func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
@@ -26,8 +26,13 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r * http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request) {
 		title := r.URL.Path[len("/view/"):]
 		p, _ := loadPage(title)
 		fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
